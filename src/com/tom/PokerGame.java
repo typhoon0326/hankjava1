c34c2a4720fe6e08a6bce9e10c35b84954ab7ffe
typhoon@gmail.com <user@es302n12>
2018-11-27 12:04:15 +0800

    initial

--- src/com/tom/PokerGame.java
+++ src/com/tom/PokerGame.java
@@ -19,6 +19,7 @@ public class PokerGame
 		
 		System.out.println(flowers.length);
 		System.out.println(nums.length);
+		
 		for(int i=0; i<nums.length; i++)
 			{
 				System.out.println(nums[i]);
